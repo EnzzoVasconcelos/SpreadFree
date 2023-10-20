e04dbd2bbea41326dd6e3e6ca7bad6d6bd0e8da1
SpreadFreeTCC <enzzovasconcelos44@gmail.com>
2023-10-20 00:00:12 -0300

    certo

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 var session = require("express-session");
+const port = process.env.PORT || '5000';
+
 
 app.use(
     session(
@@ -22,33 +24,6 @@ app.use(
 var rotas = require("./app/routes/router");
 app.use("/", rotas);
 
-/*
-const posts = [
-  { id: 1, title: 'Post 1', content: 'Conteúdo do Post 1' },
-  { id: 2, title: 'Post 2', content: 'Conteúdo do Post 2' },
-  { id: 3, title: 'Post 3', content: 'Conteúdo do Post 3' },
-];
-
-app.get('/comunidade', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Feed</title>
-    </head>
-    <body>
-      <h1>Feed de Posts</h1>
-      <ul>
-        ${posts.map(post => `<li><strong>${post.title}</strong>: ${post.content}</li>`).join('')}
-      </ul>
-    </body>
-    </html>
-  `);
-});*/
-
-
-
-
-app.listen(5500, () =>{
-    console.log(`Servidor ouvindo na porta 5500`);
+app.listen(port, () =>{
+    console.log(`Servidor ouvindo na porta 5000`);
 });
